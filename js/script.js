bd496bbc507711f14d61fa76d01536ad2903fbad
harshuu989 <80062050+harshuu989@users.noreply.github.com>
2022-02-06 10:03:40 +0530

    second commit

--- js/script.js
+++ js/script.js
@@ -17,22 +17,40 @@ var swiper = new Swiper(".home-slider", {
         loop:true,
       });
 
-      var header = document.getElementById("navbarSupportedContent");
-      var btns = header.getElementsByClassName("lis");
-      for (var i = 0; i < btns.length; i++) {
-        btns[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-        });
-      }
+      // var header = document.getElementById("products-name");
+      // var btns = header.getElementsByClassName("btnn");
+      // for (var i = 0; i < btns.length; i++) {
+      //   btns[i].addEventListener("click", function() {
+      //   var current = document.getElementsByClassName("list-active");
+      //   current[0].className = current[0].className.replace(" list-active", "");
+      //   this.className += " list-active";
+      //   });
+      // }
+      $(document).ready(function()
+      {
+        $('.list').click(function(){
+          const value=$(this).attr('data-filter');
+          if(value=='all')
+          {
+            $('.shower').show('1000');
+          }
+          else {
+            $('.shower').not('.'+value).hide('1000');
+            $('.shower').filter('.'+value).show('1000');
+          }
+        })
+      })
 
-// navbar=document.querySelector(".navbar-collapse").querySelectorAll("a");
-// console.log(navbar);
-// navbar.forEach(elements => {
-//   elements.addEventListener("click",function(){
-//     navbar.forEach(nav => nav.classList.remove("active"))
-//     this.classList.add("active");
-//     console.log("harsh")
-//   })
-// });
+      var btnContainer = document.getElementById("size-boxes");
+
+    // Get all buttons with class="btn" inside the container
+    var btns = btnContainer.getElementsByClassName("box");
+
+    // Loop through the buttons and add the active class to the current/clicked button
+    for (var i = 0; i < btns.length; i++) {
+      btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("bactive");
+        current[0].className = current[0].className.replace(" bactive", "");
+        this.className += " bactive";
+      });
+    }
